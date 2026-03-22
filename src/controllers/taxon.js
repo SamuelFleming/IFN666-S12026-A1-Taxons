3aa9b205169f9f09e68c5b85022536d57e469328
SamuelFLeming <flemo1485@gmail.com>
2026-03-22 17:58:55 +1000

    get, getAll and create

--- src/controllers/taxon.js
+++ src/controllers/taxon.js
@@ -1,20 +1,53 @@
 const asyncHandler = require("express-async-handler");
 
-let taxons = [];
+let taxons = []
 
 exports.getAll = asyncHandler(async (req, res, next) =>{
-    res.status(501).json({msg: 'getAll (Taxon Controller) not implmemented'});
+    res.json(taxons); 
 });
 
 exports.get = asyncHandler(async (req, res, next) => {
     const { id } = req.params;
     console.log(`Controller reached with ID: ${id}`);
-    res.status(501).json({msg: 'getOne (Taxon Controller) not implmemented'});
+    if (!id) {
+        return res.status(400).json({ message: "taxonomy_id is required" });
+    }
+
+    const taxon = taxons.find((t) => t.taxonomy_id === id);
+
+    if (!taxon) {
+        return res.status(204).send();
+    }
+
+    return res.status(200).json(taxon);
 });
 
 exports.create = asyncHandler(async (req, res, next) => {
     const { scientific_name, common_name, taxonomy_id } = req.body;
-    res.status(501).json({msg: 'create (Taxon Controller) not implemented'});
+
+    if (!taxonomy_id) {
+        return res.status(400).json({ message: "taxonomy_id is required" });
+    }
+
+    if (!scientific_name || !common_name) {
+        return res.status(400).json({ message: "Required fields are missing" });
+    }
+
+    const existingTaxon = taxons.find((t) => t.taxonomy_id === taxonomy_id);
+
+    if (existingTaxon) {
+        return res.status(409).json({ message: "Taxon already exists" });
+    }
+
+    const newTaxon = {
+        scientific_name,
+        common_name,
+        taxonomy_id
+    };
+
+    taxons.push(newTaxon);
+
+    return res.status(201).json(newTaxon);
 });
 
 exports.update = asyncHandler(async (req, res, next) => {

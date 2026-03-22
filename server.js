d0f3ccfc6de6b5032a8a66071f9f5717ddc98674
SamuelFLeming <flemo1485@gmail.com>
2026-03-22 19:14:40 +1000

    Middleware at the route level

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require("express");
 
 // Put your code here
 const apiRouter = require("./src/routes/index.js");
-const mediaTypeValidator = require("./src/middleware/mediaTypeValidator");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(mediaTypeValidator);
 
 const port = 3000;
 

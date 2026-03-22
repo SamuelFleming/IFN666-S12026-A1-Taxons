f75a6bd70d6e76506b153803e8386e3e825ef29c
SamuelFLeming <flemo1485@gmail.com>
2026-03-22 13:31:37 +1000

    Adjusted to specs

--- src/controllers/taxon.js
+++ src/controllers/taxon.js
@@ -1,27 +1,31 @@
 const asyncHandler = require("express-async-handler");
 
+let taxons = [];
+
 exports.getAll = asyncHandler(async (req, res, next) =>{
     res.status(501).json({msg: 'getAll (Taxon Controller) not implmemented'});
 });
 
 exports.get = asyncHandler(async (req, res, next) => {
-    let id = req.params.id;
+    const { id } = req.params;
     console.log(`Controller reached with ID: ${id}`);
     res.status(501).json({msg: 'getOne (Taxon Controller) not implmemented'});
 });
 
 exports.create = asyncHandler(async (req, res, next) => {
+    const { scientific_name, common_name, taxonomy_id } = req.body;
     res.status(501).json({msg: 'create (Taxon Controller) not implemented'});
 });
 
 exports.update = asyncHandler(async (req, res, next) => {
-    let id = req.params.id;
-    console.log(`Controller reached with ID: ${id}`);
+    const { id } = req.params;
+    const { scientific_name, common_name } = req.body;
+    console.log(`Controller reached with ID: ${id}, and content ${scientific_name} and ${common_name}`);
     res.status(501).json({msg: 'update (Taxon Controller) not implmented'});
 });
 
 exports.delete = asyncHandler(async (req, res, next) => {
-    let id = req.params.id;
+    const { id } = req.params;
     console.log(`Controller reached with ID: ${id}`);
     res.status(501).json({msg: 'delete (Taxon Controller) not implemented'});
 });

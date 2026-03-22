49b980f8849c1151e7f65c45c962b60e8b112481
SamuelFLeming <flemo1485@gmail.com>
2026-03-22 18:24:21 +1000

    Exercise 5 complete (return without msg)

--- src/controllers/taxon.js
+++ src/controllers/taxon.js
@@ -54,18 +54,43 @@ exports.update = asyncHandler(async (req, res, next) => {
     const { id } = req.params;
     const { scientific_name, common_name } = req.body;
     console.log(`Controller reached with ID: ${id}, and content ${scientific_name} and ${common_name}`);
-    res.status(501).json({msg: 'update (Taxon Controller) not implmented'});
+    
+    if (!id) {
+        return res.status(400).json({ message: "taxonomy_id is required" });
+    }
+
+    if (!scientific_name || !common_name) {
+        return res.status(400).json({ message: "Required fields are missing" });
+    }
+
+    const taxon = taxons.find((t) => t.taxonomy_id === id);
+
+    if (!taxon) {
+        return res.status(204).send();
+    }
+
+    taxon.scientific_name = scientific_name;
+    taxon.common_name = common_name;
+
+    return res.status(200).json(taxon);
 });
 
 exports.delete = asyncHandler(async (req, res, next) => {
     const { id } = req.params;
     console.log(`Controller reached with ID: ${id}`);
-    res.status(501).json({msg: 'delete (Taxon Controller) not implemented'});
+    
+    if (!id) {
+        return res.status(400).json({ message: "taxonomy_id is required" });
+    }
+
+    const taxonIndex = taxons.findIndex((t) => t.taxonomy_id === id);
+
+    if (taxonIndex === -1) {
+        return res.status(204).send();
+    }
+
+    const deletedTaxon = taxons.splice(taxonIndex, 1)[0];
+
+    return res.status(200).json(deletedTaxon);
 });
 
-// // Here are the list of endpoints
-// GET /api/taxons to retrieve all taxons.
-// GET /api/taxons/:id to retrieve a particular taxon.
-// POST /api/taxons to create a new taxon.
-// PUT /api/taxons/:id to update an existing taxon.
-// DELETE /api/taxons/:id to remove an existing taxon.
